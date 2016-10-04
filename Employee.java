eb1e056ec9a64c5be68837bb1c9b161d4f3e5233
Akshay <akkiriyal@gmail.com>
2016-10-04 11:00:19 +0530

    My First Push

---
+++ Employee.java
@@ -0,0 +1,34 @@
+package superClassExtract;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+class Employee extends Person{
+	private Date hireDay;
+	private double salary;
+	public Employee(String n, double s, int year, int month, int day,String address,int age) {
+	    name = n;
+	    salary = s;
+	    GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
+	    //  0 J
+	    hireDay = calendar.getTime();
+	    this.address=address;
+	    this.age=age;
+	}
+
+	  public Date getHireDay() {
+		  
+	    return hireDay;
+	  
+	  }
+	   public void raiseSalary(double byPercent) {
+	    double raise = salary * byPercent / 100;
+	    salary += raise;
+	  }
+	public  void print()
+	   {
+		  System.out.println("Name:"+name+"\nSalary:"+salary+"\nDOJ:"+hireDay+"\nAddress:"+address+"\nAge:"+age); 
+	   }
+
+			
+}
